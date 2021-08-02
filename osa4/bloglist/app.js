6b019f6dce0cb045d4095db8e6362e7463ab7334
Pndy <Pndy@users.noreply.github.com>
2021-08-02 19:49:25 +0300

    4.2

---
+++ osa4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const blogRouter = require('./controllers/blogs')
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((err) => {
+        logger.error('Error connecting to MongoDB: ', err.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
