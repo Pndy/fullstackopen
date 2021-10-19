0272e3d74d98348e51d0b9d7d9608f350335a535
Pndy <Pndy@users.noreply.github.com>
2021-10-19 20:34:41 +0300

    5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,22 +3,22 @@ import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
 
+let blogData = {
+  id: '616d36f1a247d6c8b0544c49',
+  title: 'New blogpost',
+  author: 'Author',
+  url: 'http://localhost/blog/1',
+  likes: 12,
+  user: {
+    id: '616c4886935621a4049c533e',
+    username: 'user',
+    name: 'User user'
+  }
+}
+
 describe('<Blog /> component', () => {
 
   test('should only show title and author by default', () => {
-    let blogData = {
-      id: '616d36f1a247d6c8b0544c49',
-      title: 'New blogpost',
-      author: 'Author',
-      url: 'http://localhost/blog/1',
-      likes: 12,
-      user: {
-        id: '616c4886935621a4049c533e',
-        username: 'user',
-        name: 'User user'
-      }
-    }
-
     const likeBlog = jest.fn()
     const deleteBlog = jest.fn()
 
@@ -31,6 +31,24 @@ describe('<Blog /> component', () => {
 
     expect(component.container).not.toHaveTextContent(blogData.url)
     expect(component.container).not.toHaveTextContent(`likes: ${blogData.likes}`)
+  })
+
+  test('also shows url and likes when show more is pressed', () => {
+    const likeBlog = jest.fn()
+    const deleteBlog = jest.fn()
+
+    let component = render(
+      <Blog blog={blogData} likeBlog={likeBlog} deleteBlog={deleteBlog} />
+    )
+
+    expect(component.container).not.toHaveTextContent(blogData.url)
+    expect(component.container).not.toHaveTextContent(`likes: ${blogData.likes}`)
+
+    const showhideBtn = component.getByText('show')
+    fireEvent.click(showhideBtn)
+
+    expect(component.container).toHaveTextContent(blogData.url)
+    expect(component.container).toHaveTextContent(`likes: ${blogData.likes}`)
   })
 
 
