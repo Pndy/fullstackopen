48b190f8a1b52b7122a8381ebceeecfc0a4d3cc5
Pndy <Pndy@users.noreply.github.com>
2021-10-19 12:58:38 +0300

    5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -6,8 +6,13 @@ import loginService from './services/login'
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -31,6 +36,7 @@ const App = () => {
         username, password
       })
       window.localStorage.setItem('user', JSON.stringify(newUser))
+      blogService.setToken(newUser.token)
       setUser(newUser)
       setUsername("")
       setPassword("")
@@ -41,7 +47,24 @@ const App = () => {
 
   const handleLogout = async() => {
     window.localStorage.removeItem('user')
+    blogService.setToken(null)
     setUser(null)
+  }
+
+  const handleNewPost = async(e) => {
+    e.preventDefault()
+
+    try{
+      const newPost = await blogService.create({
+        title, author, url
+      })
+      setBlogs(blogs.concat(newPost))
+      setTitle("")
+      setAuthor("")
+      setUrl("")
+    }catch(err){
+      console.log(err)
+    }
   }
 
   if (user === null) {
@@ -76,6 +99,41 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.username} logged in <button onClick={handleLogout}>logout</button></p>
+
+      <div>
+        <form onSubmit={handleNewPost} >
+          <h2>Add new blog</h2>
+          <div>
+            title:
+            <input 
+              type="text"
+              value={title}
+              name="title"
+              onChange={({target}) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author:
+            <input 
+              type="text"
+              value={author}
+              name="author"
+              onChange={({target}) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url:
+            <input 
+              type="text"
+              value={url}
+              name="url"
+              onChange={({target}) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">Add new</button>
+        </form>
+      </div>
+      <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,12 +1,29 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+const create = async details => {
+  const config = {
+    headers: { 'Authorization': token }
+  }
+
+  const response = await axios.post(baseUrl, details, config)
+  return response.data
+}
+
 const blogService = {
-  getAll
+  setToken,
+  getAll,
+  create,
 }
 export default blogService
