057914c26f43956a492e27696aef0b0e81cad544
Pndy <Pndy@users.noreply.github.com>
2021-10-19 16:09:55 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisibility] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisibility(!visible)
+  }
+
+  return (
+    <div className="blogentry">
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+      {visible ?
+      <>
+        <br />{blog.url}
+        <br />likes: {blog.likes} <button>like</button>
+        <br />{blog.user.name}
+      </>
+      : null
+      }
   </div>  
-)
+  )
+}
 
 export default Blog
