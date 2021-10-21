fb6bd4bc60032fb1532aec694f05f56e95a894b4
Pndy <Pndy@users.noreply.github.com>
2021-10-21 23:20:32 +0300

    7.12

--- osa7/bloglist/bloglist-frontend/src/App.js
+++ osa7/bloglist/bloglist-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Blog from './components/Blog'
@@ -6,17 +6,14 @@ import BlogAddForm from './components/BlogAddForm'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
-import blogService from './services/blogs'
-import loginService from './services/login'
 
-import { setNotification } from './reducers/notificationReducer'
 import { initBlogs } from './reducers/blogReducer'
+import { initUser, login, logout } from './reducers/userReducer'
 
 const App = () => {
-  const [user, setUser] = useState(null)
-
   const dispatch = useDispatch()
   const blogs = useSelector(state => state.blogs)
+  const user = useSelector(state => state.user)
 
   const blogformRef = useRef()
 
@@ -27,41 +24,28 @@ const App = () => {
   useEffect(() => {
     const userJson = window.localStorage.getItem('user')
     if(userJson){
-      const newUser = JSON.parse(userJson)
-      setUser(newUser)
-      blogService.setToken(newUser.token)
+      dispatch(initUser(JSON.parse(userJson)))
     }
   }, [])
 
   const handleLogin = async (details) => {
-    try{
-      const newUser = await loginService.login(details)
-      window.localStorage.setItem('user', JSON.stringify(newUser))
-      blogService.setToken(newUser.token)
-      setUser(newUser)
-      dispatch(setNotification(`Welcome ${newUser.name}`, 'info', 5))
-    }catch(err){
-      dispatch(setNotification('Incorrect login details', 'error', 5))
-    }
+    dispatch(login(details))
   }
 
   const handleLogout = async() => {
-    window.localStorage.removeItem('user')
-    blogService.setToken(null)
-    setUser(null)
-    dispatch(setNotification('Logged out!', 'info', 5))
+    dispatch(logout())
   }
 
   return (
     <div>
       <h1>Blogs</h1>
-      {user === null ? null :
+      {Object.keys(user).length === 0 ? null :
         <div>
           <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
         </div>
       }
       <Notification />
-      {user === null ?
+      {Object.keys(user).length === 0 ?
         <LoginForm
           handleLogin={handleLogin}
         />
---
+++ osa7/bloglist/bloglist-frontend/src/reducers/userReducer.js
@@ -0,0 +1,58 @@
+import loginService from '../services/login'
+import blogService from '../services/blogs'
+
+import { setNotification } from './notificationReducer'
+
+export const login = (details) => {
+  return async dispatch => {
+    try{
+      const newUser = await loginService.login(details)
+      window.localStorage.setItem('user', JSON.stringify(newUser))
+      blogService.setToken(newUser.token)
+      dispatch({
+        type: 'LOGIN_USER',
+        data: newUser
+      })
+      dispatch(setNotification(`Welcome ${newUser.name}`, 'info', 5))
+    }catch(err){
+      dispatch(setNotification('Incorrect login details', 'error', 5))
+    }
+  }
+}
+
+export const logout = () => {
+  return async dispatch => {
+    window.localStorage.removeItem('user')
+    blogService.setToken(null)
+    dispatch({
+      type: 'LOGOUT_USER',
+    })
+    dispatch(setNotification('Logged out!', 'info', 5))
+  }
+}
+
+export const initUser = (user) => {
+  return async dispatch => {
+    dispatch({
+      type: 'INIT_USER',
+      data: user
+    })
+    blogService.setToken(user.token)
+  }
+}
+
+
+const reducer = (state = {}, action) => {
+  switch(action.type){
+    case 'LOGIN_USER':
+      return action.data
+    case 'LOGOUT_USER':
+      return {}
+    case 'INIT_USER':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export default reducer
