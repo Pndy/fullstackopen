e9594f1f1d119c12d01bd55df2ff7e631feecbf4
Pndy <Pndy@users.noreply.github.com>
2021-10-26 15:04:24 +0300

    8.9

--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -3,10 +3,20 @@ import { gql } from '@apollo/client'
 export const ALL_AUTHORS = gql`
   query {
     allAuthors {
-      name,
-      born,
-      bookCount,
-      id
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      genres
     }
   }
 `
